e55197ab7eae45405d19b73721ba0b1e9691a3bb
Wolfgang Bund <wb@dtp-net.de>
2021-08-15 16:42:47 +0200

    Vorschlag für Medialink

--- boot.php
+++ boot.php
@@ -30,4 +30,13 @@ if (rex::isBackend() && rex::getUser()) {
         rex_extension::register('REX_FORM_CONTROL_FIELDS', ['\TinyMCE5\Handler\TinyMCE5ExtensionHandler', 'removeDemoControlFields'], rex_extension::LATE);
         rex_extension::register(['REX_FORM_SAVED', 'REX_FORM_DELETED', 'TINY5_PROFILE_CLONE', 'TINY5_PROFILE_DELETE', 'TINY5_PROFILE_ADD', 'TINY5_PROFILE_UPDATED'], ['\TinyMCE5\Handler\TinyMCE5ExtensionHandler', 'createProfiles']);
     }
+    if (rex_request('page') == 'mediapool/media' && rex_request('addon') == 'tiny5') {
+        rex_extension::register('OUTPUT_FILTER',function($ep) {
+            $subject = $ep->getSubject();
+            $subject = str_replace('</form>','<input type="hidden" name="addon" value="tiny5"></form>',$subject);
+            return str_replace('selectMedia','selectLink',$subject);
+        });
+        rex_view::addJsFile($addon->getAssetsUrl('js/rex5tinymce.js'));
+    }
+	
 }
---
+++ pages/insertlink.php
@@ -0,0 +1,4 @@
+<p><a href="index.php?page=mediapool/media&addon=tiny5&args[types]=jpg%2Cjpeg%2Cpng%2Cgif%2Cbmp%2Ctiff%2Csvg&opener_input_field=REX_MEDIA_tinymce5_medialink">Medienpool</a></p>
+<?php
+require_once rex_path::addon('structure','pages/linkmap.php');
+?>
