0f59670dc9c1bec7457b2aeca4b29912bd8cf040
Tobias Krais <tk@design-to-use.de>
2022-01-29 15:03:32 +0100

    Link für Medien korrigiert

--- assets/js/rex5tinymce.js
+++ assets/js/rex5tinymce.js
@@ -1,7 +1,6 @@
 var selectLink = function(filename,alt) {
     var event = opener.jQuery.Event("rex:selectLink");
     console.log($(window.opener).find('.tox-textfield[type=url]'));
-    window.opener.document.getElementsByClassName("tox-textfield")[0].value = '/media/download/'+filename;
+    window.opener.document.getElementsByClassName("tox-textfield")[0].value = '/media/'+filename;
     self.close();
 }
-
