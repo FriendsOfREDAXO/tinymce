ed80dbb66e6df1e347dbc4ca99e3b0d0db4322fc
Tobias Krais <tk@design-to-use.de>
2022-01-29 16:04:05 +0100

    Profile beim Update erneut initialisieren
    Sollte diese beiden Bugs beheben:
    https://github.com/FriendsOfREDAXO/tinymce5/issues/27
    https://github.com/FriendsOfREDAXO/tinymce5/issues/42

--- update.php
+++ update.php
@@ -4,3 +4,10 @@ if (rex_version::compare($this->getVersion(), '1.2.4', '<')) {
     $result = rex_sql::factory();
     $result->setQuery('UPDATE `' . \rex::getTablePrefix() . 'tinymce5_profiles` SET `extra` = CONCAT(`extra`, ",\r\nrelative_urls : false,\r\nremove_script_host : true,\r\ndocument_base_url : \'/\',\r\nconvert_urls : true,\r\n\r\nrel_list: [\r\n {title: \'Keine\', value: \'\'},\r\n {title: \'Nofollow\', value: \'nofollow\'}\r\n],\r\n");');
 }
+
+// Recreate profiles
+try {
+    TinyMCE5ProfilesCreator::profilesCreate();
+}
+catch(\rex_functional_exception $e) {
+}
