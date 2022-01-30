bc4904656bd77a469e8a09aa9fbdc6a715c970c4
Tobias Krais <tk@design-to-use.de>
2022-01-30 16:29:33 +0100

    Fehlerhafte Konfiguration entfernen
    Fehlerhafte Konfigurationen (siehe https://github.com/FriendsOfREDAXO/tinymce5/issues/15) aus bestehenden Konfigurationen entfernen.

--- update.php
+++ update.php
@@ -4,7 +4,12 @@ if (rex_version::compare($this->getVersion(), '1.2.5', '<')) {
     $result = rex_sql::factory();
     $result->setQuery('UPDATE `' . \rex::getTablePrefix() . 'tinymce5_profiles` SET `extra` = CONCAT("relative_urls : false,\r\nremove_script_host : true,\r\ndocument_base_url : \'/\',\r\nconvert_urls : true,\r\n\r\nrel_list: [\r\n {title: \'Keine\', value: \'\'},\r\n {title: \'Nofollow\', value: \'nofollow\'}\r\n],\r\n", `extra`);');
 }
-
+if (rex_version::compare($this->getVersion(), '1.2.7', '<')) {
+    // Update profiles to fix https://github.com/FriendsOfREDAXO/tinymce5/issues/15
+    $result = rex_sql::factory();
+    $result->setQuery('UPDATE `' . \rex::getTablePrefix() . 'tinymce5_profiles` SET `extra` = REPLACE(`extra`, ",\r\ninit_instance_callback: function (theEditor) {\r\n    rex5_init_callback(theEditor);\r\n},\r\nsetup: function (theEditor) {\r\n    rex5_setup_callback(theEditor);\r\n}", "");');
+}
+REPLACE(dimensionen," ", "")
 // Recreate profiles
 $addon = rex_addon::get('tinymce5');
 $addon->setConfig('update_profiles', true);
