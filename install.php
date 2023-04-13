4f5ae9885d6f15862b5e6bb7168ac5223ca0b38a
Thomas Skerbis <thomas.skerbis@mac.com>
2023-04-13 21:18:59 +0200

    deactivate tinymce if available

--- install.php
+++ install.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author mail[at]doerr-softwaredevelopment[dot]com Joachim Doerr
  * @package redaxo5
@@ -7,16 +8,24 @@
 
 /** @var rex_addon $this */
 $addon = rex_addon::get("tinymce");
-$old_table_name = rex::getTable('tinymce5_profiles');
-$new_table_name = rex::getTable('tinymce_profiles');
+if (rex_addon::get('tinymce')->isAvailable() && !$addon->hasConfig('migrated')) {
+    $old_table_name = rex::getTable('tinymce5_profiles');
+    $new_table_name = rex::getTable('tinymce_profiles');
 
-// duplicate Table from tinymce5 to 6 if available
-$tiny5table = rex_sql::factory()->setQuery('SHOW TABLES LIKE "'.$old_table_name.'"' )->getRows();
-$tinytable = rex_sql::factory()->setQuery('SHOW TABLES LIKE "'.$new_table_name.'"' )->getRows();
-if ($tiny5table && !$tinytable) {
-    rex_sql::factory()->setQuery('CREATE TABLE '. $new_table_name .' LIKE '. $old_table_name);
-    rex_sql::factory()->setQuery('INSERT INTO `'.$new_table_name.'` SELECT * FROM `'.$old_table_name.'`');
-    $addon->setProperty('successmsg', '<br><strong>' . rex_i18n::msg("tinymce_migration_message") . '</strong>');
+    // deactivate tiny5 addon
+    $addon = rex_addon::get('tinymce5');
+    $package_manager = rex_package_manager::factory($addon);
+    $package_manager->deactivate();
+
+
+    // duplicate Table from tinymce5 to 6 if available
+    $tiny5table = rex_sql::factory()->setQuery('SHOW TABLES LIKE "' . $old_table_name . '"')->getRows();
+    $tinytable = rex_sql::factory()->setQuery('SHOW TABLES LIKE "' . $new_table_name . '"')->getRows();
+    if ($tiny5table && !$tinytable) {
+        rex_sql::factory()->setQuery('CREATE TABLE ' . $new_table_name . ' LIKE ' . $old_table_name);
+        rex_sql::factory()->setQuery('INSERT INTO `' . $new_table_name . '` SELECT * FROM `' . $old_table_name . '`');
+        $addon->setProperty('successmsg', '<br><strong>' . rex_i18n::msg("tinymce_migration_message") . '</strong>');
+    }
 }
 
 
